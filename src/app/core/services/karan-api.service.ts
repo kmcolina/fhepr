fc6691cdb1381cd3faaf105a9ddc908461a0f8fb
kmcolina <kathymcolina@gmail.com>
2021-07-02 07:33:53 -0400

    enviro1

--- src/app/core/services/karan-api.service.ts
+++ src/app/core/services/karan-api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {apiKaran} from "Karan-Front/src/environments/environment";
+import {apiKaran} from "./Karan-Front/src/environments/environment";
 import {Observable} from "rxjs";
 
 @Injectable({
